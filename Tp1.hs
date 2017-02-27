b5107ac9d1483ed956e52af3a8f443ebf63c1f53
LouNeto <lou.g.n@hotmail.com>
2017-02-27 10:58:20 -0500

    Jai remplace le fichier de base par le fichier de depart du prof

--- Tp1.hs
+++
@@ -1,3 +0,0 @@
---TP1 dans le cadre du cours INF2160
---Lou-Gomes Neto et Pier-Olivier Decoste
---NETL14039105, DECP09059005
---
+++ miniTex.hs
@@ -0,0 +1,15 @@
+--TP1 dans le cadre du cours INF2160
+--Lou-Gomes Neto et Pier-Olivier Decoste
+--NETL14039105, DECP09059005
+import System.Environment(getArgs)
+
+-- fonction que vous devez completer.
+-- pour l'instant elle retourne la chaine recu en argument,
+-- vous devez changer la valeur de retour pour le resultat
+-- de votre programme.
+miniTex :: String -> String
+miniTex ss = ss
+
+main = do arguments <- getArgs
+          contenuFichier <- readFile (head arguments)
+          putStr (miniTex contenuFichier)
